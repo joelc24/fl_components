af5c68af8feef80722e78008004c6a72a60c93b7
Joel <libra-marriaga@hotmail.com>
2022-04-05 17:19:08 -0500

    Cupertino Dialog

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,10 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog(BuildContext context){
+  void displayDialogIOS(BuildContext context){
+    showCupertinoDialog(
+      context: context, 
+      builder: (context) => CupertinoAlertDialog(
+        title: const Text('Alerta'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const <Widget>[
+            Text('Este es el contenido de la alerta'), 
+            SizedBox(height: 10),
+            FlutterLogo(size: 100),
+          ],
+        ),
+        actions: <Widget>[
+          CupertinoDialogAction(
+            child: const Text('Cancelar', style: TextStyle(color: Colors.red)),
+            onPressed: () => Navigator.pop(context),
+          ),
+          CupertinoDialogAction(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      )
+    );
+  }
+
+  void displayDialogAndroid(BuildContext context){
     showDialog(
       barrierDismissible: false,
       context: context,
@@ -48,7 +80,9 @@ class AlertScreen extends StatelessWidget {
              shape: const StadiumBorder(),
              elevation: 0,
            ), */
-           onPressed: ()=> displayDialog(context),
+           
+           onPressed: ()=> Platform.isAndroid ? displayDialogAndroid(context) : displayDialogIOS(context),
+           
             child: const Padding(
               padding:  EdgeInsets.symmetric(horizontal: 20, vertical:  15),
               child:  Text('Mostrar Alerta', style: TextStyle(fontSize: 20),),
