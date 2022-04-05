bf9f5437f522dd9be44bb8fc0f4e71c8ae5fcb88
Joel <libra-marriaga@hotmail.com>
2022-04-05 16:55:38 -0500

    Mostrar una Alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,6 +3,40 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
+
+  void displayDialog(BuildContext context){
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          title: const Text('Alerta'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+          content:Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const <Widget>[
+              Text('Este es un mensaje de alerta'),
+              SizedBox(height: 20),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: const Text('Aceptar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -14,7 +48,7 @@ class AlertScreen extends StatelessWidget {
              shape: const StadiumBorder(),
              elevation: 0,
            ), */
-           onPressed: (){},
+           onPressed: ()=> displayDialog(context),
             child: const Padding(
               padding:  EdgeInsets.symmetric(horizontal: 20, vertical:  15),
               child:  Text('Mostrar Alerta', style: TextStyle(fontSize: 20),),
@@ -23,9 +57,7 @@ class AlertScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+        onPressed: () => Navigator.of(context).pop(),
       ),
     );
   }
