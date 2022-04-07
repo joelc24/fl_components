e71d2a3062fbf480cfcd50a07a515c38e1511021
Joel <libra-marriaga@hotmail.com>
2022-04-07 00:12:15 -0500

    TextFormField

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -10,18 +10,21 @@ class AppRoutes {
   static const String initialRoute = 'home';
 
   static final menuOpcion = <MenuOpcion>[
-    MenuOpcion(route: 'home', icon: Icons.home, name: 'Home', screen: const HomeScreen()),
+    /* MenuOpcion(route: 'home', icon: Icons.home, name: 'Home', screen: const HomeScreen()), */
     MenuOpcion(route: 'listview1', icon: Icons.list_alt, name: 'Listview Tipo 1', screen: const ListView1Screen()),
     MenuOpcion(route: 'listview2', icon: Icons.list, name: 'Listview Tipo 2', screen: const ListView2Screen()),
     MenuOpcion(route: 'alert', icon: Icons.add_alert_rounded, name: 'Alertas', screen: const AlertScreen()),
     MenuOpcion(route: 'Card', icon: Icons.card_giftcard, name: 'Cards', screen: const CardScreen()),
-    MenuOpcion(route: 'Avatar', icon: Icons.supervised_user_circle_outlined, name: 'Circle Avatar', screen: const AvatarScreen()),
+    MenuOpcion(route: 'avatar', icon: Icons.supervised_user_circle_outlined, name: 'Circle Avatar', screen: const AvatarScreen()),
+     MenuOpcion(route: 'animate', icon: Icons.play_circle_outline, name: 'Animated Container', screen: const AnimatedScreen()),
+     MenuOpcion(route: 'inputs', icon: Icons.input_rounded, name: 'Text Inputs', screen: const InputsScreen()),
 
   ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
     
     Map<String, Widget Function(BuildContext)> appRoutes = {};
+    appRoutes.addAll({'home': (BuildContext context) => const HomeScreen()});
 
     for (final opcion in menuOpcion) {
       appRoutes.addAll({opcion.route: (BuildContext context) => opcion.screen});
