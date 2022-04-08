331850f3257ed8879ea99a75f5bcbebfef12fe49
Joel <libra-marriaga@hotmail.com>
2022-04-07 22:07:16 -0500

    Centralizar el estilo de los inputs

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/widgets/custom_input_field.dart';
 import 'package:flutter/material.dart';
 
 class InputsScreen extends StatelessWidget {
@@ -14,41 +15,9 @@ class InputsScreen extends StatelessWidget {
         child: Padding( 
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
-            children: <Widget>[
+            children: const <Widget>[
 
-              TextFormField(
-                autofocus: true,
-                initialValue: '',
-                textCapitalization: TextCapitalization.words,
-                onChanged: ( value ){
-                  print('value $value');
-                },
-                validator: (value){
-                  if(value == null){
-                    return 'Este Campo es obligatorio';
-                  }
-                  return value.length < 3 ? 'Este Campo debe tener al menos 3 caracteres' : null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: const InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.indigo, width: 2),
-                  ),
-                  hintText: 'Nombre del Usuario',
-                  labelText: 'Nombre',
-                  helperText: 'Solo letras',
-                  counterText: '3 caacteres',
-                  /* prefixIcon: Icon(Icons.person), */
-                  suffixIcon: Icon(Icons.group_add_outlined),
-                  icon: Icon(Icons.assignment_ind_outlined),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                  )
-              ),
-              )
+              CustomInputField(labelText: 'Nombre', hintText: 'Nombre del Usuario')
             ]
           )
         ),
@@ -56,3 +25,4 @@ class InputsScreen extends StatelessWidget {
     );
   }
 }
+
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -32,6 +32,23 @@ class AppTheme {
             elevation: 0,
           ),
         ),
+
+        inputDecorationTheme: const InputDecorationTheme(
+          floatingLabelStyle: TextStyle(color: primary),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: primary, width: 2),
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10))
+          ),
+
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: primary, width: 2),
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10))
+          ),
+
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), topRight: Radius.circular(10))
+          ),
+        ),
   );
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
